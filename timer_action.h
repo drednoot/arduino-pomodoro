d897533c4563055d181beda3f2054635ee93acdc
unknown <matveyerohin228@gmail.com>
2025-11-17 23:13:59 +0300

    feat: added template timer that performs action on sync

---
+++ timer_action.h
@@ -0,0 +1,69 @@
+#ifndef ARDUINO_POMODORO_TIMER_ACTION_H_
+#define ARDUINO_POMODORO_TIMER_ACTION_H_
+
+namespace detail {
+
+class TimerActionImpl {
+  public:
+    TimerActionImpl(uint32_t timer)
+      : m_timer(timer)
+      , m_lastSnapshot(0)
+    {
+    }
+
+    void sync()
+    {
+      if (millis() - m_lastSnapshot >= m_timer) {
+        m_lastSnapshot = millis();
+        callAction();
+      }
+    }
+
+  protected:
+    virtual void callAction() = 0;
+
+    uint32_t m_timer;
+    uint32_t m_lastSnapshot;
+};
+
+} // namespace detail
+
+template<typename Action, typename Caller = void>
+class TimerAction : public detail::TimerActionImpl {
+  public:
+  TimerAction(uint32_t timer, Caller caller, Action action)
+    : detail::TimerActionImpl(timer)
+    , m_caller(caller)
+    , m_action(action)
+  {
+  }
+
+  virtual void callAction() override
+  {
+    (m_caller->*m_action)();
+  }
+
+  private:
+    Caller m_caller;
+    Action m_action;
+};
+
+template<typename Action>
+class TimerAction<Action, void> : public detail::TimerActionImpl {
+  public:
+  TimerAction(uint32_t timer, Action action) 
+    : detail::TimerActionImpl(timer)
+    , m_action(action)
+  {
+  }
+
+  virtual void callAction() override
+  {
+    m_action();
+  }
+
+  private:
+    Action m_action;
+};
+
+#endif // ARDUINO_POMODORO_TIMER_ACTION_H_
