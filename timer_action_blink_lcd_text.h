2f3f4a57121c9ddfdc5f9fcdfe77f128a4ee7532
unknown <matveyerohin228@gmail.com>
2025-11-20 23:32:36 +0300

    feat: add text blinking on full reset

---
+++ timer_action_blink_lcd_text.h
@@ -0,0 +1,38 @@
+#ifndef ARDUINO_POMODORO_TIMER_ACTION_BLINK_LCD_TEXT_H_
+#define ARDUINO_POMODORO_TIMER_ACTION_BLINK_LCD_TEXT_H_
+
+#include "ns_timer_action.h"
+#include "lcd_timer.h"
+
+class BlinkLcdText;
+
+using BlinkLcdTextAction = TimerAction<void(BlinkLcdText::*)(), BlinkLcdText*>;
+
+class BlinkLcdText : public BlinkLcdTextAction {
+  public:
+    BlinkLcdText(LcdTimer* lcdTimer, uint32_t time)
+      : BlinkLcdTextAction(time, this, &BlinkLcdText::blink)
+      , m_lcdTimer(lcdTimer)
+      , m_state(true)
+    {
+    }
+
+    void reset()
+    {
+      m_state = true;
+      m_lcdTimer->setTextVisible(true);
+      BlinkLcdTextAction::reset();
+    }
+
+  private:
+    void blink()
+    {
+      m_state = !m_state;
+      m_lcdTimer->setTextVisible(m_state);
+    }
+
+    boolean m_state;
+    LcdTimer* m_lcdTimer;
+};
+
+#endif // ARDUINO_POMODORO_TIMER_ACTION_BLINK_LCD_TEXT_H_
